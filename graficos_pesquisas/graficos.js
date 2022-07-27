83fb6c4db9d470929ef405fbab8a07cdb1c271aa
Victor G. Marques <86068797+VictorGM01@users.noreply.github.com>
2022-07-26 21:40:52 -0300

    Adição do código padrão para gráfico de colunas

--- graficos_pesquisas/graficos.js
+++ graficos_pesquisas/graficos.js
@@ -19,6 +19,29 @@ google.charts.load("current", {packages:["corechart"]});
       // slices: {num. da label: {offset: 0.2}},
     };
 
-    var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+    var chart = new google.visualization.PieChart(document.getElementById('perguntaX'));
+    chart.draw(data, options);
+}
+
+// Gráfico de Colunas
+
+google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(drawChart14);
+    function drawChart14() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Questao');
+    data.addColumn('number', 'Respostas');
+    data.addRows([
+      ["Sim", 1], // exemplo
+    ]);
+
+    var options = {
+      colors: [''],  // cor das colunas
+      bars: 'vertical',
+      backgroundColor: "#4a8450",
+      legend: "none",
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('perguntaXY'));
     chart.draw(data, options);
 }
